94250eaf1019fd70e2ac4c70e2303e65064a8eb3
NomanSadick <nomansadickjs@gmail.com>
2022-10-18 19:57:55 +0600

    some tricky interview questions solve

---
+++ interview/questions-2.js
@@ -0,0 +1,9 @@
+// what will be the output of below code?
+function myFunction(){
+    console.log(arguments);
+    return 'Bangladesh';
+    
+}
+
+const string = myFunction `hello `;
+console.log(string); // Bangladesh
---
+++ interview/questions-7.js
@@ -0,0 +1,4 @@
+// what will be the output of below code?
+console.log(("Bangladesh").__proto__); // Strings
+console.log(("Bangladesh").__proto__.__proto__); // Object
+console.log(("Bangladesh").__proto__.__proto__.__proto__); // null
